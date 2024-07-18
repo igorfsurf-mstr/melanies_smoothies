e15287dc379aced48aea003bd517ed17abc0974e
Igu <140611329+igorfsurf-mstr@users.noreply.github.com>
2024-07-18 10:50:13 +0200

    Update streamlit_app.py
    Expose the JSON Data Inside the Response Object

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,6 +46,7 @@ if ingredient_list:
         #st.write('Your Smoothie is ordered!', name_on_order, icon="✅")
         st.success(f'Your Smoothie is ordered, {name_on_order}!',  icon="✅")
 
+#New section to display fruityvice nutrition information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
